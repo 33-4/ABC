875ee4ee28dc3de432d82e1a36bd4a714f20e2f3
33-4 <yu-ueki@aori.u-tokyo.ac.jp>
2020-08-26 15:25:41 +0900

    8/22

---
+++ c176.f90
@@ -0,0 +1,32 @@
+program c176
+
+implicit none
+integer :: n, i, m, x
+integer, allocatable :: a(:)
+
+read *, n
+
+if(n == 1) then
+ print *, 0
+ stop
+end if
+
+allocate(a(1:n))
+
+x = 0
+
+read *, a(:)
+
+do i = 1, n-1
+ m = a(i) - a(i+1)
+ if(m < 0) then
+  x = x
+ else
+  x = x + m
+  a(i+1) = a(i+1) + m
+ end if
+end do
+
+print *, x
+
+end program c176
